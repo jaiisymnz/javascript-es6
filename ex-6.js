286935e7a9e74452cc932b4408d09976e61e93a2
Orayanee Janjaeng <orayaneejj@gmail.com>
2024-10-15 14:12:07 +0700

    answer: ex1-7

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,9 @@
 let height = undefined;
 let result;
 
+function checkHeight(height) {
+  result = height ?? "Height is not defined";
+  return result;
+}
+result = checkHeight(height);
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
